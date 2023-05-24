5b0452146b4c6b3c88aeb8555d93a438125ea9d9
Coby <contact@45930.xyz>
2023-05-23 21:51:12 -0400

    adding new proofs

--- src/phase/PhaseProof.ts
+++ src/phase/PhaseProof.ts
@@ -1,10 +1,17 @@
-import { Experimental, SelfProof, Signature, UInt32 } from 'snarkyjs';
+import {
+  Experimental,
+  PrivateKey,
+  SelfProof,
+  Signature,
+  UInt32,
+} from 'snarkyjs';
 import { PhaseState } from './PhaseState';
 import { Piece } from '../objects/Piece';
 import { Action } from '../objects/Action';
 import { PiecesMerkleWitness } from '../objects/PiecesMerkleTree';
 import { ArenaMerkleWitness } from '../objects/ArenaMerkleTree';
 import { Position } from '../objects/Position';
+import { EncrytpedAttackRoll } from '../objects/AttackDiceRolls';
 
 export const PhaseProgram = Experimental.ZkProgram({
   publicInput: PhaseState,
@@ -52,6 +59,90 @@ export const PhaseProgram = Experimental.ZkProgram({
           assertedMoveDistance
         );
         stateAfterMove.assertEquals(newState);
+      },
+    },
+    applyRangedAttack: {
+      privateInputs: [
+        SelfProof,
+        Action,
+        Signature,
+        Piece,
+        Piece,
+        PiecesMerkleWitness,
+        PiecesMerkleWitness,
+        UInt32,
+        EncrytpedAttackRoll,
+        PrivateKey,
+      ],
+      method(
+        newState: PhaseState,
+        oldPhaseProof: SelfProof<PhaseState>,
+        action: Action,
+        actionSignature: Signature,
+        attackingPiece: Piece,
+        targetPiece: Piece,
+        attackingPieceWitness: PiecesMerkleWitness,
+        targetPieceWitness: PiecesMerkleWitness,
+        assertedAttackDistance: UInt32,
+        attackRoll: EncrytpedAttackRoll,
+        serverSecretKey: PrivateKey
+      ) {
+        oldPhaseProof.verify();
+        const stateAfterAttack =
+          oldPhaseProof.publicInput.applyRangedAttackAction(
+            action,
+            actionSignature,
+            attackingPiece,
+            targetPiece,
+            attackingPieceWitness,
+            targetPieceWitness,
+            assertedAttackDistance,
+            attackRoll,
+            serverSecretKey
+          );
+        stateAfterAttack.assertEquals(newState);
+      },
+    },
+    applyMeleeAttack: {
+      privateInputs: [
+        SelfProof,
+        Action,
+        Signature,
+        Piece,
+        Piece,
+        PiecesMerkleWitness,
+        PiecesMerkleWitness,
+        UInt32,
+        EncrytpedAttackRoll,
+        PrivateKey,
+      ],
+      method(
+        newState: PhaseState,
+        oldPhaseProof: SelfProof<PhaseState>,
+        action: Action,
+        actionSignature: Signature,
+        attackingPiece: Piece,
+        targetPiece: Piece,
+        attackingPieceWitness: PiecesMerkleWitness,
+        targetPieceWitness: PiecesMerkleWitness,
+        assertedAttackDistance: UInt32,
+        attackRoll: EncrytpedAttackRoll,
+        serverSecretKey: PrivateKey
+      ) {
+        oldPhaseProof.verify();
+        const stateAfterAttack =
+          oldPhaseProof.publicInput.applyMeleeAttackAction(
+            action,
+            actionSignature,
+            attackingPiece,
+            targetPiece,
+            attackingPieceWitness,
+            targetPieceWitness,
+            assertedAttackDistance,
+            attackRoll,
+            serverSecretKey
+          );
+        stateAfterAttack.assertEquals(newState);
       },
     },
   },
