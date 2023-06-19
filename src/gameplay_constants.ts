7fb04266e82a18ab49a48326d9466d2f1e19defd
Louis Wheeler <louiswheeleriv@gmail.com>
2023-06-19 17:41:06 -0400

    Increase melee range

--- src/gameplay_constants.ts
+++ src/gameplay_constants.ts
@@ -1,4 +1,4 @@
 import { UInt32 } from 'snarkyjs';
 
-export const MELEE_ATTACK_RANGE = 24;
+export const MELEE_ATTACK_RANGE = 50;
 export const MELEE_ATTACK_RANGE_U32 = UInt32.from(MELEE_ATTACK_RANGE);
