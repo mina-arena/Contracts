e580b42f4b9e2a5ae39fecc88c3772bf3724b215
Coby <contact@45930.xyz>
2023-06-22 09:19:11 -0400

    update gameplay constants

--- src/gameplay_constants.ts
+++ src/gameplay_constants.ts
@@ -3,8 +3,8 @@ import { UInt32 } from 'snarkyjs';
 export const MELEE_ATTACK_RANGE = 24;
 export const MELEE_ATTACK_RANGE_U32 = UInt32.from(MELEE_ATTACK_RANGE);
 
-export const ARENA_WIDTH = 800;
-export const ARENA_HEIGHT = 800;
+export const ARENA_WIDTH = 650;
+export const ARENA_HEIGHT = 550;
 
 export const ARENA_WIDTH_U32 = UInt32.from(ARENA_WIDTH);
 export const ARENA_HEIGHT_U32 = UInt32.from(ARENA_HEIGHT);
